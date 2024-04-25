dedfd6a96e3b065215dd3b8ec4d11c7f1b791cd8
Ven Bui <vab337@nyu.edu>
2024-04-25 18:13:06 +0700

    second push

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
-function setup() {
-  createCanvas(400, 400);
+async function getData() {
+  const response = await fetch('./data.json');
+  const data = await response.json();
+  console.log(data);
+
+  console.log(Object.keys(data).length);
+
+  for(let i=0; i<Object.keys(data).length ; i++) {
+    var typeList = document.getElementById('type'+ (i+1) + 'List');
+ 
+    var typeName = Object.keys(data)[i];
+    var typeHead = document.getElementById('type'+ (i+1) + 'Head');
+    typeHead.textContent = typeName;
+    
+    data[typeName].forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = `${item.name} - $${item.price}`;
+      typeList.appendChild(li);
+  })
+}
 }
 
-function draw() {
-  background(220);
-}
+getData();
